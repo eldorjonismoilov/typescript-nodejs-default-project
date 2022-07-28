852c60f4245aee1a86e3dc3df5db9d084f5166a3
Ishonch <ishonchoffice@gmail.com>
2022-07-28 15:31:38 +0500

    Default project

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import server from './src/server'
 
 dotenv.config()
 
-const { HOST, PORT } = process.env
+const { HOST, PORT }  = process.env
 
 server.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
